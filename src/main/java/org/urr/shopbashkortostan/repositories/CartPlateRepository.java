32f2718336af2a8a7ec5c66905199be1577512af
m004ka <ysupov959@gmail.com>
2024-03-03 00:05:43 +0300

    Создание Листов для запросов по параметрам

--- src/main/java/org/urr/shopbashkortostan/repositories/CartPlateRepository.java
+++ src/main/java/org/urr/shopbashkortostan/repositories/CartPlateRepository.java
@@ -6,6 +6,24 @@ import org.springframework.stereotype.Repository;
 import org.urr.shopbashkortostan.models.Account;
 import org.urr.shopbashkortostan.models.CartStove;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CartPlateRepository extends JpaRepository<CartStove, Long> {
+    List<CartStove> findAllByHeightAndSize(float height, CartStove.Size size);
+    List<CartStove> findAllByHeight(float height);
+
+    List<CartStove> findAllBySize(CartStove.Size size);
+
+    List<CartStove> findAllByDensity(int density);
+
+
+
+
+
+    CartStove findCartStoveByPriceAndDensityAndHeightAndText(BigDecimal price, int density, float height, String Text);
+    Optional<CartStove>findCartStoveByHeight(float height);
+
 }
