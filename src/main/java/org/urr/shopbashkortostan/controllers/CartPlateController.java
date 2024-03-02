e29e7e7621a04f55a22a91245dba05b0f05563c7
m004ka <ysupov959@gmail.com>
2024-03-03 00:06:16 +0300

    Создание PostMapping для создания и удаления карты товара

--- src/main/java/org/urr/shopbashkortostan/controllers/CartPlateController.java
+++ src/main/java/org/urr/shopbashkortostan/controllers/CartPlateController.java
@@ -1,12 +1,33 @@
 package org.urr.shopbashkortostan.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.urr.shopbashkortostan.dto.CartForm;
+import org.urr.shopbashkortostan.service.interfaces.CreateNewCartService;
+import org.urr.shopbashkortostan.service.interfaces.DeleteCartService;
 
 @RestController
 @RequestMapping("/createCard")
 @RequiredArgsConstructor
 public class CartPlateController {
+    CreateNewCartService createNewCartService;
+    DeleteCartService deleteCartService;
 
+    @PostMapping("/newCart")
+    ResponseEntity<?> CreateCart(CartForm cartForm){
+        createNewCartService.CreateCart(cartForm);
+        return ResponseEntity.ok("Success body");
+    }
+    @PostMapping("/deleteCart")
+    ResponseEntity<?> DeleteCart(CartForm cartForm){ //Да да я знаю что это колхоз
+        boolean res = deleteCartService.DeleteCart(cartForm);
+        if (res){
+            return ResponseEntity.ok("Success body");
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
