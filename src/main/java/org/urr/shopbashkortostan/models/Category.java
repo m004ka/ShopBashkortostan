6b73c62433ae08871e7eef4e37c4fd5a98d920bb
m004ka <ysupov959@gmail.com>
2024-03-09 23:49:43 +0300

    Категории

---
+++ src/main/java/org/urr/shopbashkortostan/models/Category.java
@@ -0,0 +1,21 @@
+package org.urr.shopbashkortostan.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+
+}
