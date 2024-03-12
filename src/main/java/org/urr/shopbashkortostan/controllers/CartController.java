f612f2ed6537e5b694944f798eccee61d47b39e2
m004ka <ysupov959@gmail.com>
2024-03-12 23:01:55 +0300

    Контроллер и Дто для добавления товара в корзину

--- src/main/java/org/urr/shopbashkortostan/controllers/CartController.java
+++ src/main/java/org/urr/shopbashkortostan/controllers/CartController.java
@@ -5,10 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.urr.shopbashkortostan.dto.AddCartDTO;
+import org.urr.shopbashkortostan.models.Account;
 import org.urr.shopbashkortostan.models.Product;
+import org.urr.shopbashkortostan.repositories.ProductRepository;
+import org.urr.shopbashkortostan.service.Impl.AccountService;
 import org.urr.shopbashkortostan.service.Impl.CartService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/carts")
@@ -18,10 +23,14 @@ public class CartController {
 
 
     private final CartService cartService;
+    private final AccountService accountService;
+    private final ProductRepository productRepository;
 
     @PostMapping("/add")
-    public void addToCart(@RequestBody Product product, @RequestParam int quantity, Authentication authentication) {
-       cartService.addToCart(product, quantity, authentication);
+    public void addToCart(@RequestBody AddCartDTO addCartDTO, Authentication authentication) {
+        Account account = accountService.getAccountFromAuthentication(authentication).orElseThrow(()-> new RuntimeException("Аккаунт не найден"));
+        Product product = productRepository.getProductById(addCartDTO.getId());
+        cartService.addToCart(product, addCartDTO.getQuantity(), account);
     }
 
     @DeleteMapping("/remove")
