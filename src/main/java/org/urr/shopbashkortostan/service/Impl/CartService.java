b807455ce4688f7391f105742f99bd80592facf0
m004ka <ysupov959@gmail.com>
2024-03-21 20:43:34 +0300

    Переработка маппингов под реализованный service корзины

--- src/main/java/org/urr/shopbashkortostan/service/Impl/CartService.java
+++ src/main/java/org/urr/shopbashkortostan/service/Impl/CartService.java
@@ -11,11 +11,11 @@ import org.urr.shopbashkortostan.models.Cart;
 import org.urr.shopbashkortostan.models.CartItem;
 import org.urr.shopbashkortostan.models.Product;
 import org.urr.shopbashkortostan.repositories.AccountRepository;
-import org.urr.shopbashkortostan.repositories.CartItemRepository;
 import org.urr.shopbashkortostan.repositories.CartRepository;
 import org.urr.shopbashkortostan.repositories.ProductRepository;
 
 import java.io.Console;
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,43 +25,94 @@ public class CartService {
 
 
     private final CartRepository cartRepository; // Репозиторий для работы с корзинами
-    private final CartItemServiceImpl cartItemService;
     private final AccountService accountService;
-    private final CartItemRepository cartItemRepository;
     private final ProductRepository productRepository;
+
 
     @Transactional
     public void addToCart(Product product, int quantity, Account account) {
-        Cart cart = cartRepository.findByAccountId(account.getId()).orElseThrow(() -> new RuntimeException("Корзина не найдена"));
-        Hibernate.initialize(cart.getCartItems());
-        CartItem cartItem = CartItem.builder()
-                .product(product)
-                .quantity(quantity)
-                .build();
-        cartItemRepository.save(cartItem);
+        Cart cart = account.getCart();
+        List<CartItem> cartItems = cart.getCartItems();
+        CartItem cartItem = cartItems.stream().filter(p -> {
 
-        cart.getCartItems().add(cartItem);
+            return p.getProduct().getId().equals(product.getId());
+        }).findFirst().orElse(CartItem.builder().product(product).build());
+        cartItem.setQuantity(cartItem.getQuantity() + quantity);
+        if (!cartItems.contains(cartItem)) cartItems.add(cartItem);
+
         cartRepository.save(cart);
+        getCartWeight(account);
+        getCartTotal(account);
     }
 
-    public void removeFromCart(Long productId) {
-        // Логика удаления товара из корзины в базе данных
+    public void removeFromCart(Long productId, Account account) {
+        Cart cart = account.getCart();
+        List<CartItem> cartItems = cart.getCartItems();
+        CartItem cartItem = cartItems.stream().filter(p -> {
+            System.out.println("\n\n\n\n АЙДИ " + p.getId() + "\n\n");
+            return p.getProduct().getId().equals(productId);
+        }).findFirst().orElseThrow(() -> new RuntimeException("item not found with id: " + productId));
+        cartItems.remove(cartItem);
+        cartRepository.save(cart);
+        getCartWeight(account);
+        getCartTotal(account);
     }
 
-    public void updateCartItemQuantity(Long productId, int quantity) {
-        // Логика обновления количества товара в корзине в базе данных
+    public void updateCartItemQuantity(Long productId, int quantity, Account account) {
+        Cart cart = account.getCart();
+        List<CartItem> cartItems = cart.getCartItems();
+        CartItem cartItem = cartItems.stream().filter(p -> {
+            // System.out.println("\n\n\n\n АЙДИ " + p.getId() + "\n\n");
+            return p.getProduct().getId().equals(productId);
+        }).findFirst().orElseThrow(() -> new RuntimeException("item not found with id: " + productId));
+        cartItem.setQuantity(cartItem.getQuantity() + quantity);
+        cartRepository.save(cart);
+        getCartWeight(account);
+        getCartTotal(account);
     }
 
-//    public List<CartItem> getCartItems(Long cartId) {
-//        // Логика получения списка товаров в корзине из базы данных
-//    }
-
-    public void clearCart(Long cartId) {
-        // Логика очистки корзины в базе данных
+    public List<CartItem> getCartItems(Account account) {
+        Cart cart = account.getCart();
+        return cart.getCartItems();
     }
 
-//    public BigDecimal getCartTotal(Long cartId) {
-//        // Логика расчета общей стоимости товаров в корзине из базы данных
-//        return
-//    }
+    public void clearCart(Account account) {
+        Cart cart = account.getCart();
+        cart.setCartItems(List.of());
+        cartRepository.save(cart);
+        getCartWeight(account);
+        getCartTotal(account);
+    }
+
+    public BigDecimal getCartTotal(Account account) {
+        Cart cart = account.getCart();
+        BigDecimal total = BigDecimal.valueOf(0);
+        int quantity = 0;
+        BigDecimal price = BigDecimal.valueOf(0);
+        List<CartItem> cartItems = cart.getCartItems();
+        for (CartItem elem : cartItems) {
+            quantity = elem.getQuantity();
+            price = elem.getProduct().getPrice();
+            total = total.add(price.multiply(BigDecimal.valueOf(quantity)));
+        }
+        cart.setTotalAmount(total);
+        cartRepository.save(cart);
+        return total;
+    }
+
+    public BigDecimal getCartWeight(Account account) {
+        Cart cart = account.getCart();
+        BigDecimal totalweight = BigDecimal.valueOf(0);
+        int quantity = 0;
+        BigDecimal weight = BigDecimal.valueOf(0);
+        List<CartItem> cartItems = cart.getCartItems();
+        for (CartItem elem : cartItems) {
+            quantity = elem.getQuantity();
+            weight = elem.getProduct().getWeight();
+            totalweight = totalweight.add(weight.multiply(BigDecimal.valueOf(quantity)));
+        }
+        cart.setTotalWeight(totalweight);
+        cartRepository.save(cart);
+        return totalweight;
+    }
 }
