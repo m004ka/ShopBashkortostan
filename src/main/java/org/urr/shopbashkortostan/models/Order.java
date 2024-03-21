af9ead9ff8c7d9549c626193716a799802ba29bf
m004ka <ysupov959@gmail.com>
2024-03-21 20:45:18 +0300

    Переделка модели заказа

--- src/main/java/org/urr/shopbashkortostan/models/Order.java
+++ src/main/java/org/urr/shopbashkortostan/models/Order.java
@@ -2,10 +2,13 @@ package org.urr.shopbashkortostan.models;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.urr.shopbashkortostan.enums.Address;
 import org.urr.shopbashkortostan.enums.OrderStatus;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -19,18 +22,26 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private Long orderId;
+
     @ManyToOne
     Account account;
 
+
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    List<CartItem> cartItems;
+
     private BigDecimal exactAmount;
 
-    private BigDecimal amountWithVAT;
+    private BigDecimal exactWeight;
 
-    private Date orderDate;
+    private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
-    private String address;
+    private Address address;
 
 }
