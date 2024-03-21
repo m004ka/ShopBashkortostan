50e596b8ff31243ae3f75bdfbf03ba4eb87b2612
m004ka <ysupov959@gmail.com>
2024-03-21 20:43:03 +0300

    Удаление за ненадобностью

--- src/main/java/org/urr/shopbashkortostan/repositories/CartItemRepository.java
+++
@@ -1,7 +0,0 @@
-package org.urr.shopbashkortostan.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.urr.shopbashkortostan.models.CartItem;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-}
